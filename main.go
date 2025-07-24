cef679dc73665cc37ff13824dc6edc03455b7a09
Ciaran McAleer <72049678+CiaranMcAleer@users.noreply.github.com>
2025-07-24 12:22:39 +0000

    rebuild initial cli in cobra

--- main.go
+++ main.go
@@ -4,33 +4,29 @@ package main
 import (
 	"fmt"
 	"os"
+
+	"github.com/spf13/cobra"
 )
 
-var version = "dev"// Version set during build with go build -ldflags "-X main.version=1.2.3"
+var version = "dev" // Version set during build with go build -ldflags "-X main.version=1.2.3"
 
 func main() {
-	//switch to handdle command line arguments
-	if len(os.Args) > 1 {
-		switch os.Args[1] {
-		case "--help":
-			help()
-		case "--version":
-			fmt.Println("Version:", version)
-			os.Exit(0)
-		default:
-			fmt.Println("Unknown command:", os.Args[1])
-			help()
-		}
-	} else {
-		fmt.Println("No arguments provided. Use --help for usage information.")
+	rootCmd := &cobra.Command{
+		Use:   "colade",
+		Short: "Colade - Static site generator from Markdown",
+		Long:  `Colade is a CLI tool to generate static sites from Markdown files.`,
+	}
+
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "version",
+		Short: "Show the version of Colade",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Colade version:", version)
+		},
+	})
+
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Println(err)
 		os.Exit(1)
 	}
-}
-
-func help() {
-	fmt.Println("Usage: go run main.go [options]")
-	fmt.Println("Options:")
-	fmt.Println("  --help\tShow this help message")
-	fmt.Println("  --version\tShow the version of the CLI")
-	os.Exit(0)
 }
