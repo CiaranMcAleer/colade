5c59e58b0a04c3f7d0003cd1d37ac6863959ee48
Ciaran McAleer <72049678+CiaranMcAleer@users.noreply.github.com>
2025-07-24 12:24:47 +0000

    skeleton for build command

--- main.go
+++ main.go
@@ -17,6 +17,19 @@ func main() {
 		Long:  `Colade is a CLI tool to generate static sites from Markdown files.`,
 	}
 
+	buildCmd := &cobra.Command{
+		Use:   "build [inputDir] [outputDir]",
+		Short: "Build a static site from Markdown files",
+		Args:  cobra.ExactArgs(2),
+		Run: func(cmd *cobra.Command, args []string) {
+			inputDir := args[0]
+			outputDir := args[1]
+			fmt.Printf("[Scaffold] Would build site from '%s' to '%s'...\n", inputDir, outputDir)
+		},
+	}
+
+	rootCmd.AddCommand(buildCmd)
+
 	rootCmd.AddCommand(&cobra.Command{
 		Use:   "version",
 		Short: "Show the version of Colade",
